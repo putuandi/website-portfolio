a16ca9e87730a5bc37fa86d2b1894e992bca2f94
putuandi <fergusoboy123@gmail.com>
2023-08-14 08:48:25 +0800

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+//clear contact form
+window.onbeforeunload = () => {
+    for (const form of document.getElementsByTagName("form")){
+        form.reset();
+    }
+};
+
+//toggle & responsive nav
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const navLists = document.querySelector("nav");
+
+    burger.addEventListener("click", () => {
+        navLists.classList.toggle("nav-active");
+        burger.classList.toggle("toggle-burger");
+    });
+};
+
+navSlide();
